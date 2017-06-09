4d09be8e59c938ca3431b6df9763f2a45f2f6d81
Mikel <mikel@zhenit.com>
2017-06-09 10:50:29 +0200

    [FIX] Version number for wp

--- woocommerce-sequra/gateway-sequra.php
+++ woocommerce-sequra/gateway-sequra.php
@@ -3,7 +3,7 @@
   Plugin Name: Pasarela de pago para SeQura
   Plugin URI: http://sequra.es/
   Description: Da la opción a tus clientes usar los servicios de SeQura para pagar.
-  Version: 4.1.1
+  Version: 4.2.0
   Author: SeQura Engineering
   Author URI: http://SeQura.es/
  */
