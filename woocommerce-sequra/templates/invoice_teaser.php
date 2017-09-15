8929142d36bd5e6bc3e2825bf8c5469f69f7827a
Mikel Martin <mikel@zhenit.com>
2017-09-15 16:10:19 +0200

    [add] Use templates for teasers

---
+++ woocommerce-sequra/templates/invoice_teaser.php
@@ -0,0 +1,16 @@
+<?php
+if ( $sequra->is_available() && $product->price < $sequra->max_amount ) { ?>
+      <div id="sequra_invoice_teaser"></div>
+      <script type="text/javascript">
+  jQuery(function(){
+    invoiceTeaser = new SequraInvoiceTeaser(
+      {
+        container:'#sequra_invoice_teaser',
+        fee: <?php echo $sequra->fee?$sequra->fee:0;?>
+      }
+    );
+    invoiceTeaser.draw();
+  });
+      </script>
+<?php
+}
