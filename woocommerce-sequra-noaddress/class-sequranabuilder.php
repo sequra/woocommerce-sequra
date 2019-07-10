a57aefb85991515397c13d4d94fc035effff3933
Mikel <mikel@zhenit.com>
2019-07-10 14:48:27 +0200

    [add] autoupdate

--- woocommerce-sequra-noaddress/class-sequranabuilder.php
+++ woocommerce-sequra-noaddress/class-sequranabuilder.php
@@ -1,18 +1,30 @@
 <?php
 
 class SequraNABuilder extends SequraBuilderWC {
-
+	/**
+	 * Undocumented function
+	 *
+	 * @return array
+	 */
 	public function merchant() {
-		$ret                                       = parent::merchant();
+		$ret                                        = parent::merchant();
 		$ret['options']['addresses_may_be_missing'] = true;
 
 		return $ret;
 	}
-
+	/**
+	 * Undocumented function
+	 *
+	 * @return null
+	 */
 	public function deliveryAddress() {
 		return null;
 	}
-
+	/**
+	 * Undocumented function
+	 *
+	 * @return null
+	 */
 	public function invoiceAddress() {
 		return null;
 	}
