4f6ba7907a79c4eff275969db161221cdaae2b1b
Mikel <mikel@zhenit.com>
2021-04-21 10:31:30 +0200

    [FIX] missing register activation hook

--- woocommerce-sequracheckout/woocommerce-sequracheckout.php
+++ woocommerce-sequracheckout/woocommerce-sequracheckout.php
@@ -3,11 +3,11 @@
  * Plugin Name: Checkout con SeQura
  * Plugin URI: http://sequra.es/
  * Description: Ofrece las opciones de pago de SeQura
- * Version: 1.1.0
+ * Version: 1.1.1
  * Author: SeQura Engineering
  * Author URI: http://SeQura es/
  * WC requires at least: 4.0
- * WC tested up to: 5.0
+ * WC tested up to: 5.2
  * Icon1x: https://live.sequracdn.com/assets/images/badges/invoicing.svg
  * Icon2x: https://live.sequracdn.com/assets/images/badges/invoicing_l.svg
  * BannerHigh: https://live.sequracdn.com/assets/images/logos/logo.svg
@@ -20,7 +20,7 @@
 
 //Make sure old WooCommerce SeQura is not installed
 if ( ! defined( 'WC_SEQURA_PLG_PATH' ) && ! file_exists( WP_PLUGIN_DIR . '/woocommerce-sequra' ) ) {
-	define( 'SEQURACHECKOUT_VERSION', '1.1.0' );
+	define( 'SEQURACHECKOUT_VERSION', '1.1.1' );
 	define( 'WC_SEQURA_PLG_PATH', WP_PLUGIN_DIR . '/' . basename( plugin_dir_path( __FILE__ ) ) . '/' );
 	define( 'SEQURA_PLUGIN_UPDATE_SERVER', 'https://engineering.sequra.es' );
 	require_once WC_SEQURA_PLG_PATH . 'lib/wp-package-updater/class-wp-package-updater.php';
@@ -31,6 +31,7 @@ if ( ! defined( 'WC_SEQURA_PLG_PATH' ) && ! file_exists( WP_PLUGIN_DIR . '/wooco
 		wp_normalize_path( WC_SEQURA_PLG_PATH )
 	);
 	add_filter( 'plugin_action_links_' . plugin_basename( __FILE__ ), 'sequrapayment_action_links' );
+	register_activation_hook( __FILE__, 'sequra_activation' );
 	require_once WC_SEQURA_PLG_PATH . 'gateway-sequra.php';
 } else {
 	add_action( 'admin_notices',
