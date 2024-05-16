36795373bcdf47887838a01b4bfb52a13aaa735b
Michel Escalante Álvarez <michel.escalante@sequra.es>
2024-05-16 13:41:51 +0200

    Add webpack

--- sequra/assets/js/settings.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    SequraFE.utilities.showLoader();
-    SequraFE.state = new SequraFE.StateController(SequraFE._state_controller);
-    SequraFE.state.display();
-    SequraFE.utilities.hideLoader();
-});
--- sequra/src/Controllers/class-assets-controller.php
+++ sequra/src/Controllers/class-assets-controller.php
@@ -16,31 +16,32 @@ use SeQura\WC\Services\Interface_Settings;
  */
 class Assets_Controller implements Interface_Assets_Controller {
 
+	private const HANDLE_SETTINGS_PAGE                           = 'sequra-settings';
 	private const HANDLE_CORE                                    = 'sequra-core';
-	private const HANDLE_CORE_IMAGES_PROVIDER                    = self::HANDLE_CORE . '-images-provider';
-	private const HANDLE_CORE_AJAX_SERVICE                       = self::HANDLE_CORE . '-ajax-service';
-	private const HANDLE_CORE_TRANSLATION_SERVICE                = self::HANDLE_CORE . '-translation-service';
-	private const HANDLE_CORE_TEMPLATE_SERVICE                   = self::HANDLE_CORE . '-template-service';
-	private const HANDLE_CORE_UTILITY_SERVICE                    = self::HANDLE_CORE . '-utility-service';
-	private const HANDLE_CORE_VALIDATION_SERVICE                 = self::HANDLE_CORE . '-validation-service';
-	private const HANDLE_CORE_RESPONSE_SERVICE                   = self::HANDLE_CORE . '-response-service';
-	private const HANDLE_CORE_PAGECONTROLLER_FACTORY             = self::HANDLE_CORE . '-pagecontroller-factory';
-	private const HANDLE_CORE_FORM_FACTORY                       = self::HANDLE_CORE . '-form-factory';
-	private const HANDLE_CORE_STATE_UUID_SERVICE                 = self::HANDLE_CORE . '-state-uuid-service';
-	private const HANDLE_CORE_ELEMENT_GENERATOR                  = self::HANDLE_CORE . '-element-generator';
-	private const HANDLE_CORE_MODAL_COMPONENT                    = self::HANDLE_CORE . '-modal-component';
-	private const HANDLE_CORE_DROPDOWN_COMPONENT                 = self::HANDLE_CORE . '-dropdown-component';
-	private const HANDLE_CORE_MULTI_ITEM_SELECTOR_COMPONENT      = self::HANDLE_CORE . '-multi-item-selector-component';
-	private const HANDLE_CORE_DATA_TABLE_COMPONENT               = self::HANDLE_CORE . '-data-table-component';
-	private const HANDLE_CORE_PAGE_HEADER_COMPONENT              = self::HANDLE_CORE . '-page-header-component';
-	private const HANDLE_CORE_CONNECTION_SETTINGS_FORM           = self::HANDLE_CORE . '-connection-settings-form';
-	private const HANDLE_CORE_WIDGET_SETTINGS_FORM               = self::HANDLE_CORE . '-widget-settings-form';
-	private const HANDLE_CORE_GENERAL_SETTINGS_FORM              = self::HANDLE_CORE . '-general-settings-form';
-	private const HANDLE_CORE_ORDER_STATUS_MAPPING_SETTINGS_FORM = self::HANDLE_CORE . '-order-status-mapping-settings-form';
-	private const HANDLE_CORE_STATE_CONTROLLER                   = self::HANDLE_CORE . '-state-controller';
-	private const HANDLE_CORE_ONBOARDING_CONTROLLER              = self::HANDLE_CORE . '-onboarding-controller';
-	private const HANDLE_CORE_PAYMENT_CONTROLLER                 = self::HANDLE_CORE . '-payment-controller';
-	private const HANDLE_CORE_SETTINGS_CONTROLLER                = self::HANDLE_CORE . '-settings-controller';
+	// private const HANDLE_CORE_IMAGES_PROVIDER                    = self::HANDLE_CORE . '-images-provider';
+	// private const HANDLE_CORE_AJAX_SERVICE                       = self::HANDLE_CORE . '-ajax-service';
+	// private const HANDLE_CORE_TRANSLATION_SERVICE                = self::HANDLE_CORE . '-translation-service';
+	// private const HANDLE_CORE_TEMPLATE_SERVICE                   = self::HANDLE_CORE . '-template-service';
+	// private const HANDLE_CORE_UTILITY_SERVICE                    = self::HANDLE_CORE . '-utility-service';
+	// private const HANDLE_CORE_VALIDATION_SERVICE                 = self::HANDLE_CORE . '-validation-service';
+	// private const HANDLE_CORE_RESPONSE_SERVICE                   = self::HANDLE_CORE . '-response-service';
+	// private const HANDLE_CORE_PAGECONTROLLER_FACTORY             = self::HANDLE_CORE . '-pagecontroller-factory';
+	// private const HANDLE_CORE_FORM_FACTORY                       = self::HANDLE_CORE . '-form-factory';
+	// private const HANDLE_CORE_STATE_UUID_SERVICE                 = self::HANDLE_CORE . '-state-uuid-service';
+	// private const HANDLE_CORE_ELEMENT_GENERATOR                  = self::HANDLE_CORE . '-element-generator';
+	// private const HANDLE_CORE_MODAL_COMPONENT                    = self::HANDLE_CORE . '-modal-component';
+	// private const HANDLE_CORE_DROPDOWN_COMPONENT                 = self::HANDLE_CORE . '-dropdown-component';
+	// private const HANDLE_CORE_MULTI_ITEM_SELECTOR_COMPONENT      = self::HANDLE_CORE . '-multi-item-selector-component';
+	// private const HANDLE_CORE_DATA_TABLE_COMPONENT               = self::HANDLE_CORE . '-data-table-component';
+	// private const HANDLE_CORE_PAGE_HEADER_COMPONENT              = self::HANDLE_CORE . '-page-header-component';
+	// private const HANDLE_CORE_CONNECTION_SETTINGS_FORM           = self::HANDLE_CORE . '-connection-settings-form';
+	// private const HANDLE_CORE_WIDGET_SETTINGS_FORM               = self::HANDLE_CORE . '-widget-settings-form';
+	// private const HANDLE_CORE_GENERAL_SETTINGS_FORM              = self::HANDLE_CORE . '-general-settings-form';
+	// private const HANDLE_CORE_ORDER_STATUS_MAPPING_SETTINGS_FORM = self::HANDLE_CORE . '-order-status-mapping-settings-form';
+	// private const HANDLE_CORE_STATE_CONTROLLER                   = self::HANDLE_CORE . '-state-controller';
+	// private const HANDLE_CORE_ONBOARDING_CONTROLLER              = self::HANDLE_CORE . '-onboarding-controller';
+	// private const HANDLE_CORE_PAYMENT_CONTROLLER                 = self::HANDLE_CORE . '-payment-controller';
+	// private const HANDLE_CORE_SETTINGS_CONTROLLER                = self::HANDLE_CORE . '-settings-controller';
 	
 	private const INTEGRATION_CORE_VERSION = '1.0.0';
 
@@ -194,64 +195,65 @@ class Assets_Controller implements Interface_Assets_Controller {
 			'_nonce'            => wp_create_nonce( 'wp_rest' ),
 		);
 
-		wp_register_script( self::HANDLE_CORE_IMAGES_PROVIDER, "{$this->assets_dir_url}/js/ImagesProvider.js", array(), self::INTEGRATION_CORE_VERSION, true );
-		wp_localize_script( self::HANDLE_CORE_IMAGES_PROVIDER, 'SequraFE', $sequra_fe );
-		wp_register_script( self::HANDLE_CORE_AJAX_SERVICE, "{$this->assets_dir_url}/js/AjaxService.js", array(), self::INTEGRATION_CORE_VERSION, true );
-		wp_register_script( self::HANDLE_CORE_TRANSLATION_SERVICE, "{$this->assets_dir_url}/js/TranslationService.js", array(), self::INTEGRATION_CORE_VERSION, true );
-		wp_register_script( self::HANDLE_CORE_TEMPLATE_SERVICE, "{$this->assets_dir_url}/js/TemplateService.js", array(), self::INTEGRATION_CORE_VERSION, true );
-		wp_register_script( self::HANDLE_CORE_UTILITY_SERVICE, "{$this->assets_dir_url}/js/UtilityService.js", array(), self::INTEGRATION_CORE_VERSION, true );
-		wp_register_script( self::HANDLE_CORE_VALIDATION_SERVICE, "{$this->assets_dir_url}/js/ValidationService.js", array(), self::INTEGRATION_CORE_VERSION, true );
-		wp_register_script( self::HANDLE_CORE_RESPONSE_SERVICE, "{$this->assets_dir_url}/js/ResponseService.js", array(), self::INTEGRATION_CORE_VERSION, true );
-		wp_register_script( self::HANDLE_CORE_PAGECONTROLLER_FACTORY, "{$this->assets_dir_url}/js/PageControllerFactory.js", array(), self::INTEGRATION_CORE_VERSION, true );
-		wp_register_script( self::HANDLE_CORE_FORM_FACTORY, "{$this->assets_dir_url}/js/FormFactory.js", array(), self::INTEGRATION_CORE_VERSION, true );
-		wp_register_script( self::HANDLE_CORE_STATE_UUID_SERVICE, "{$this->assets_dir_url}/js/StateUUIDService.js", array(), self::INTEGRATION_CORE_VERSION, true );
-		wp_register_script( self::HANDLE_CORE_ELEMENT_GENERATOR, "{$this->assets_dir_url}/js/ElementGenerator.js", array(), self::INTEGRATION_CORE_VERSION, true );
-		wp_register_script( self::HANDLE_CORE_MODAL_COMPONENT, "{$this->assets_dir_url}/js/ModalComponent.js", array(), self::INTEGRATION_CORE_VERSION, true );
-		wp_register_script( self::HANDLE_CORE_DROPDOWN_COMPONENT, "{$this->assets_dir_url}/js/DropdownComponent.js", array(), self::INTEGRATION_CORE_VERSION, true );
-		wp_register_script( self::HANDLE_CORE_MULTI_ITEM_SELECTOR_COMPONENT, "{$this->assets_dir_url}/js/MultiItemSelectorComponent.js", array(), self::INTEGRATION_CORE_VERSION, true );
-		wp_register_script( self::HANDLE_CORE_DATA_TABLE_COMPONENT, "{$this->assets_dir_url}/js/DataTableComponent.js", array(), self::INTEGRATION_CORE_VERSION, true );
-		wp_register_script( self::HANDLE_CORE_PAGE_HEADER_COMPONENT, "{$this->assets_dir_url}/js/PageHeaderComponent.js", array(), self::INTEGRATION_CORE_VERSION, true );
-		wp_register_script( self::HANDLE_CORE_CONNECTION_SETTINGS_FORM, "{$this->assets_dir_url}/js/ConnectionSettingsForm.js", array(), self::INTEGRATION_CORE_VERSION, true );
-		wp_register_script( self::HANDLE_CORE_WIDGET_SETTINGS_FORM, "{$this->assets_dir_url}/js/WidgetSettingsForm.js", array(), self::INTEGRATION_CORE_VERSION, true );
-		wp_register_script( self::HANDLE_CORE_GENERAL_SETTINGS_FORM, "{$this->assets_dir_url}/js/GeneralSettingsForm.js", array(), self::INTEGRATION_CORE_VERSION, true );
-		wp_register_script( self::HANDLE_CORE_ORDER_STATUS_MAPPING_SETTINGS_FORM, "{$this->assets_dir_url}/js/OrderStatusMappingSettingsForm.js", array(), self::INTEGRATION_CORE_VERSION, true );
-		wp_register_script( self::HANDLE_CORE_STATE_CONTROLLER, "{$this->assets_dir_url}/js/StateController.js", array(), self::INTEGRATION_CORE_VERSION, true );
-		wp_register_script( self::HANDLE_CORE_ONBOARDING_CONTROLLER, "{$this->assets_dir_url}/js/OnboardingController.js", array(), self::INTEGRATION_CORE_VERSION, true );
-		wp_register_script( self::HANDLE_CORE_PAYMENT_CONTROLLER, "{$this->assets_dir_url}/js/PaymentController.js", array(), self::INTEGRATION_CORE_VERSION, true );
-		wp_register_script( self::HANDLE_CORE_SETTINGS_CONTROLLER, "{$this->assets_dir_url}/js/SettingsController.js", array(), self::INTEGRATION_CORE_VERSION, true );
-		wp_register_script(
-			self::HANDLE_CORE,
-			"{$this->assets_dir_url}/js/settings.js",
-			array(
-				self::HANDLE_CORE_IMAGES_PROVIDER,
-				self::HANDLE_CORE_AJAX_SERVICE,
-				self::HANDLE_CORE_TRANSLATION_SERVICE,
-				self::HANDLE_CORE_TEMPLATE_SERVICE,
-				self::HANDLE_CORE_UTILITY_SERVICE,
-				self::HANDLE_CORE_VALIDATION_SERVICE,
-				self::HANDLE_CORE_RESPONSE_SERVICE,
-				self::HANDLE_CORE_PAGECONTROLLER_FACTORY,
-				self::HANDLE_CORE_FORM_FACTORY,
-				self::HANDLE_CORE_STATE_UUID_SERVICE,
-				self::HANDLE_CORE_ELEMENT_GENERATOR,
-				self::HANDLE_CORE_MODAL_COMPONENT,
-				self::HANDLE_CORE_DROPDOWN_COMPONENT,
-				self::HANDLE_CORE_MULTI_ITEM_SELECTOR_COMPONENT,
-				self::HANDLE_CORE_DATA_TABLE_COMPONENT,
-				self::HANDLE_CORE_PAGE_HEADER_COMPONENT,
-				self::HANDLE_CORE_CONNECTION_SETTINGS_FORM,
-				self::HANDLE_CORE_WIDGET_SETTINGS_FORM,
-				self::HANDLE_CORE_GENERAL_SETTINGS_FORM,
-				self::HANDLE_CORE_ORDER_STATUS_MAPPING_SETTINGS_FORM,
-				self::HANDLE_CORE_STATE_CONTROLLER,
-				self::HANDLE_CORE_ONBOARDING_CONTROLLER,
-				self::HANDLE_CORE_PAYMENT_CONTROLLER,
-				self::HANDLE_CORE_SETTINGS_CONTROLLER,
-			),
-			self::INTEGRATION_CORE_VERSION,
-			true 
-		);
-
+		// wp_register_script( self::HANDLE_CORE_IMAGES_PROVIDER, "{$this->assets_dir_url}/js/ImagesProvider.js", array(), self::INTEGRATION_CORE_VERSION, true );
+		// wp_localize_script( self::HANDLE_CORE_IMAGES_PROVIDER, 'SequraFE', $sequra_fe );
+		// wp_register_script( self::HANDLE_CORE_AJAX_SERVICE, "{$this->assets_dir_url}/js/AjaxService.js", array(), self::INTEGRATION_CORE_VERSION, true );
+		// wp_register_script( self::HANDLE_CORE_TRANSLATION_SERVICE, "{$this->assets_dir_url}/js/TranslationService.js", array(), self::INTEGRATION_CORE_VERSION, true );
+		// wp_register_script( self::HANDLE_CORE_TEMPLATE_SERVICE, "{$this->assets_dir_url}/js/TemplateService.js", array(), self::INTEGRATION_CORE_VERSION, true );
+		// wp_register_script( self::HANDLE_CORE_UTILITY_SERVICE, "{$this->assets_dir_url}/js/UtilityService.js", array(), self::INTEGRATION_CORE_VERSION, true );
+		// wp_register_script( self::HANDLE_CORE_VALIDATION_SERVICE, "{$this->assets_dir_url}/js/ValidationService.js", array(), self::INTEGRATION_CORE_VERSION, true );
+		// wp_register_script( self::HANDLE_CORE_RESPONSE_SERVICE, "{$this->assets_dir_url}/js/ResponseService.js", array(), self::INTEGRATION_CORE_VERSION, true );
+		// wp_register_script( self::HANDLE_CORE_PAGECONTROLLER_FACTORY, "{$this->assets_dir_url}/js/PageControllerFactory.js", array(), self::INTEGRATION_CORE_VERSION, true );
+		// wp_register_script( self::HANDLE_CORE_FORM_FACTORY, "{$this->assets_dir_url}/js/FormFactory.js", array(), self::INTEGRATION_CORE_VERSION, true );
+		// wp_register_script( self::HANDLE_CORE_STATE_UUID_SERVICE, "{$this->assets_dir_url}/js/StateUUIDService.js", array(), self::INTEGRATION_CORE_VERSION, true );
+		// wp_register_script( self::HANDLE_CORE_ELEMENT_GENERATOR, "{$this->assets_dir_url}/js/ElementGenerator.js", array(), self::INTEGRATION_CORE_VERSION, true );
+		// wp_register_script( self::HANDLE_CORE_MODAL_COMPONENT, "{$this->assets_dir_url}/js/ModalComponent.js", array(), self::INTEGRATION_CORE_VERSION, true );
+		// wp_register_script( self::HANDLE_CORE_DROPDOWN_COMPONENT, "{$this->assets_dir_url}/js/DropdownComponent.js", array(), self::INTEGRATION_CORE_VERSION, true );
+		// wp_register_script( self::HANDLE_CORE_MULTI_ITEM_SELECTOR_COMPONENT, "{$this->assets_dir_url}/js/MultiItemSelectorComponent.js", array(), self::INTEGRATION_CORE_VERSION, true );
+		// wp_register_script( self::HANDLE_CORE_DATA_TABLE_COMPONENT, "{$this->assets_dir_url}/js/DataTableComponent.js", array(), self::INTEGRATION_CORE_VERSION, true );
+		// wp_register_script( self::HANDLE_CORE_PAGE_HEADER_COMPONENT, "{$this->assets_dir_url}/js/PageHeaderComponent.js", array(), self::INTEGRATION_CORE_VERSION, true );
+		// wp_register_script( self::HANDLE_CORE_CONNECTION_SETTINGS_FORM, "{$this->assets_dir_url}/js/ConnectionSettingsForm.js", array(), self::INTEGRATION_CORE_VERSION, true );
+		// wp_register_script( self::HANDLE_CORE_WIDGET_SETTINGS_FORM, "{$this->assets_dir_url}/js/WidgetSettingsForm.js", array(), self::INTEGRATION_CORE_VERSION, true );
+		// wp_register_script( self::HANDLE_CORE_GENERAL_SETTINGS_FORM, "{$this->assets_dir_url}/js/GeneralSettingsForm.js", array(), self::INTEGRATION_CORE_VERSION, true );
+		// wp_register_script( self::HANDLE_CORE_ORDER_STATUS_MAPPING_SETTINGS_FORM, "{$this->assets_dir_url}/js/OrderStatusMappingSettingsForm.js", array(), self::INTEGRATION_CORE_VERSION, true );
+		// wp_register_script( self::HANDLE_CORE_STATE_CONTROLLER, "{$this->assets_dir_url}/js/StateController.js", array(), self::INTEGRATION_CORE_VERSION, true );
+		// wp_register_script( self::HANDLE_CORE_ONBOARDING_CONTROLLER, "{$this->assets_dir_url}/js/OnboardingController.js", array(), self::INTEGRATION_CORE_VERSION, true );
+		// wp_register_script( self::HANDLE_CORE_PAYMENT_CONTROLLER, "{$this->assets_dir_url}/js/PaymentController.js", array(), self::INTEGRATION_CORE_VERSION, true );
+		// wp_register_script( self::HANDLE_CORE_SETTINGS_CONTROLLER, "{$this->assets_dir_url}/js/SettingsController.js", array(), self::INTEGRATION_CORE_VERSION, true );
+		// wp_register_script(
+		// self::HANDLE_CORE,
+		// "{$this->assets_dir_url}/js/settings.js",
+		// array(
+		// self::HANDLE_CORE_IMAGES_PROVIDER,
+		// self::HANDLE_CORE_AJAX_SERVICE,
+		// self::HANDLE_CORE_TRANSLATION_SERVICE,
+		// self::HANDLE_CORE_TEMPLATE_SERVICE,
+		// self::HANDLE_CORE_UTILITY_SERVICE,
+		// self::HANDLE_CORE_VALIDATION_SERVICE,
+		// self::HANDLE_CORE_RESPONSE_SERVICE,
+		// self::HANDLE_CORE_PAGECONTROLLER_FACTORY,
+		// self::HANDLE_CORE_FORM_FACTORY,
+		// self::HANDLE_CORE_STATE_UUID_SERVICE,
+		// self::HANDLE_CORE_ELEMENT_GENERATOR,
+		// self::HANDLE_CORE_MODAL_COMPONENT,
+		// self::HANDLE_CORE_DROPDOWN_COMPONENT,
+		// self::HANDLE_CORE_MULTI_ITEM_SELECTOR_COMPONENT,
+		// self::HANDLE_CORE_DATA_TABLE_COMPONENT,
+		// self::HANDLE_CORE_PAGE_HEADER_COMPONENT,
+		// self::HANDLE_CORE_CONNECTION_SETTINGS_FORM,
+		// self::HANDLE_CORE_WIDGET_SETTINGS_FORM,
+		// self::HANDLE_CORE_GENERAL_SETTINGS_FORM,
+		// self::HANDLE_CORE_ORDER_STATUS_MAPPING_SETTINGS_FORM,
+		// self::HANDLE_CORE_STATE_CONTROLLER,
+		// self::HANDLE_CORE_ONBOARDING_CONTROLLER,
+		// self::HANDLE_CORE_PAYMENT_CONTROLLER,
+		// self::HANDLE_CORE_SETTINGS_CONTROLLER,
+		// ),
+		// self::INTEGRATION_CORE_VERSION,
+		// true 
+		// );
+		wp_register_script( self::HANDLE_SETTINGS_PAGE, "{$this->assets_dir_url}/js/settings-page.js", array(), $this->assets_version, true );
+		wp_localize_script( self::HANDLE_SETTINGS_PAGE, 'SequraFE', $sequra_fe );
 		wp_enqueue_script( self::HANDLE_CORE );
 	}
 
