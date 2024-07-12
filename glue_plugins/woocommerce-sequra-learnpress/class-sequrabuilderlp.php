1fe7035db633cc85693d6d3ee241259ba88efb63
Mikel Martin <mikel.martin@sequra.es>
2024-07-12 14:55:10 +0200

    Update learnpress sequra plugin

--- glue_plugins/woocommerce-sequra-learnpress/class-sequrabuilderlp.php
+++ glue_plugins/woocommerce-sequra-learnpress/class-sequrabuilderlp.php
@@ -18,12 +18,13 @@ class SequraBuilderLP extends SequraBuilderWC {
 	 * @param [type] $cart_item_key
 	 * @return void
 	 */
-	protected function getProductFromItem( $cart_item, $cart_item_key ) {
-		$product = parent::getProductFromItem( $cart_item, $cart_item_key);
-		if (!$product && $order = learn_press_get_order_by_item_id( $cart_item->get_id() ) ) {
-			$course_id = learn_press_get_order_item_meta( $cart_item->get_id(), '_course_id' );
-			$product = new LP_Course($course_id);
-		}
+	protected function getProductFromItem( $cart_item ) {
+        try {
+            return parent::getProductFromItem( $cart_item );
+        } catch (Exception $e) {
+            $course_id = learn_press_get_order_item_meta( $cart_item['product_id'], '_course_id' );
+            $product = new LP_Course($course_id);
+        }
 		return $product;
 	}
 
@@ -37,15 +38,19 @@ class SequraBuilderLP extends SequraBuilderWC {
 		$cart_contents = $this->getCartContents();
 		foreach ( $cart_contents as $cart_item_key => $cart_item ) {
 			$item     = array();
+			$_product = $this->getProductFromItem( $cart_item );
+			if ( ! $_product ) {
+				continue;
+			}
 			if (
-				$this->_pm->core_settings['enable_for_virtual'] == 'yes' &&
-				(get_post_meta( $cart_item->get_id(), 'is_sequra_service', true ) != 'no' ||
+				$this->pm->settings['enable_for_virtual'] == 'yes' &&
+				(get_post_meta( $cart_item['product_id'], 'is_sequra_service', true ) != 'no' ||
 				get_class($_product) == 'LP_Course'
 				)
 			) {
-				$service_end_date = get_post_meta( $cart_item->get_id(), 'sequra_service_end_date', true );
+				$service_end_date = get_post_meta( $cart_item['product_id'], 'sequra_service_end_date', true );
 				if(!SequraHelper::validate_service_date($service_end_date)){
-					$service_end_date = $this->_pm->core_settings['default_service_end_date'];
+					$service_end_date = $this->pm->settings['default_service_end_date'];
 				}
 				$item["type"] = 'service';
 				if ( strpos( $service_end_date, "P" )===0 ) {
@@ -56,8 +61,8 @@ class SequraBuilderLP extends SequraBuilderWC {
 			} else {
 				$item["type"] = 'product';
 			}
-			$item["reference"] = $cart_item->get_id();
-			$name              = $cart_item->get_name();
+			$item["reference"] = $cart_item['product_id'];
+			$name              = $cart_item['product_id'];
 			$item["name"]      = strip_tags( $name );
 			if ( isset( $cart_item['quantity'] ) ) {
 				$item["quantity"] = (int) $cart_item['quantity'];
@@ -70,44 +75,10 @@ class SequraBuilderLP extends SequraBuilderWC {
 			$item["downloadable"]   = false;//$_product->is_downloadable();
 
 			// OPTIONAL
-			$item["description"] = strip_tags( self::notNull( get_post( $cart_item->get_id() )->post_content ) );
-			$item["product_id"]  = self::notNull( $cart_item->get_id() );
-			$item["url"]         = (string) self::notNull( get_permalink( $cart_item->get_id() ) );
+			$item["description"] = strip_tags( self::notNull( get_post( $cart_item['product_id'] )->post_content ) );
+			$item["product_id"]  = self::notNull( $cart_item['product_id'] );
+			$item["url"]         = (string) self::notNull( get_permalink( $cart_item['product_id'] ) );
 			//$item["category"]    = self::notNull( strip_tags( $_product->get_categories() ) );
-			/*@TODO: $item["manufacturer"] but it is not wooCommerce stantdard attribute*/
-			$items[] = $item;
-		}
-
-		//order discounts
-		if ( $this->_current_order instanceof SequraTempOrder ) {
-			foreach ( $this->_cart->coupon_discount_amounts as $key => $val ) {
-				$amount  = $val + $this->_cart->coupon_discount_tax_amounts[ $key ];
-				$items[] = $this->discount( $key, $amount );
-			}
-		} else {
-			foreach ( $this->_current_order->get_items( 'coupon' ) as $key => $val ) {
-				$amount  = $val['discount_amount'] + $val['discount_amount_tax'];
-				$items[] = $this->discount( $val['name'], $amount );
-			}
-		}
-
-		//add Customer fee (without tax)
-		$item = array();
-		if ( $this->_current_order instanceof SequraTempOrder ) {
-			$fees = $this->_cart->fees;
-		} else {
-			$fees = $this->_current_order->get_fees();
-		}
-		foreach ( $fees as $fee_key => $fee ) {
-			$item["type"] = 'invoice_fee';
-			if ( $this->_current_order instanceof SequraTempOrder ) {
-				$item["total_with_tax"] = self::integerPrice( $fee->amount );
-				if ( $fee->tax ) {
-					$item["total_with_tax"] += self::integerPrice( $fee->tax );
-				}
-			} else {
-				$item["total_with_tax"] = self::integerPrice( $fee['line_total'] );
-			}
 			$items[] = $item;
 		}
 
--- setup.sh
+++ setup.sh
@@ -2,5 +2,5 @@
 docker run --rm --interactive --tty \
   --volume $PWD/sequra:/app \
   composer install
-docker-compose up -d
-docker-compose exec -u www-data web /bin/bash -c "/tmp/setup_woocommerce.sh"
+docker compose up -d
+docker compose exec -u www-data web /bin/bash -c "/tmp/setup_woocommerce.sh"
