feffc6bdabd37a499b072552587e4365564faef5
Michel Escalante Álvarez <michel.escalante@sequra.com>
2024-07-26 11:36:35 +0200

    Add e2e test for Excluded Categories

--- sequra/tests-e2e/fixtures/GeneralSettingsPage.js
+++ sequra/tests-e2e/fixtures/GeneralSettingsPage.js
@@ -32,6 +32,24 @@ export default class GeneralSettingsPage extends SettingsPage {
         await this.expect(this.page.locator(this.selector.allowedIPAddresses.hiddenInput), '"Allowed IP addresses" should be empty').toHaveValue('');
     }
 
+    /**
+     * @returns {import('@playwright/test').Locator}
+     */
+    #getExcludedCategoriesLocator() {
+        return this.page.locator('.sq-field-wrapper').filter({ hasText: 'Excluded categories' }).first().locator('.sq-label-wrapper + div').first();
+    }
+
+    /**
+     * @returns {import('@playwright/test').Locator}
+     */
+    #getExcludedCategoriesInputHiddenLocator() {
+        return this.#getExcludedCategoriesLocator().locator('.sqp-hidden-input').first()
+    }
+
+    async expectExcludedCategoriesToBeEmpty() {
+        await this.expect(this.#getExcludedCategoriesInputHiddenLocator(), '"Excluded Categories" should be empty').toHaveValue('');
+    }
+
     async getPublicIP() {
         const response = await this.request.get('https://checkip.amazonaws.com/');
         const publicIP = await response.text();
@@ -61,6 +79,32 @@ export default class GeneralSettingsPage extends SettingsPage {
             value += '' === value ? address : ',' + address;
             await this.expect(hiddenInputLocator, '"Allowed IP addresses" should have value "' + value + '"').toHaveValue(value);
             await this.expect(inputLocator, '"Allowed IP addresses" input field should be empty').toHaveValue('');
+        }
+    }
+
+    /**
+     * @param {string[]} excludedCategories
+     */
+    async selectExcludedCategories(excludedCategories) {
+        const selectLocator = this.#getExcludedCategoriesLocator();
+
+        // Clear previous values
+        const itemsRmBtnLocator = selectLocator.locator('.sqp-selected-item > .sqp-remove-button');
+
+        while ((await itemsRmBtnLocator.count()) > 0) {
+            await itemsRmBtnLocator.first().click();
+        }
+
+        await this.expectExcludedCategoriesToBeEmpty();
+
+        if (excludedCategories) {
+            await selectLocator.click();
+            for (const category of excludedCategories) {
+                const listItemLocator = selectLocator.getByText(category);
+                await listItemLocator.waitFor({ state: 'visible' });
+                await listItemLocator.click();
+                await selectLocator.locator('.sqp-selected-item').filter({ hasText: category }).waitFor({ state: 'visible' });
+            }
         }
     }
 
--- sequra/tests-e2e/specs/004-configuration-general.spec.js
+++ sequra/tests-e2e/specs/004-configuration-general.spec.js
@@ -20,12 +20,12 @@ test.describe('Configuration', () => {
       ['a.b.c.d', '1.1.1.256'],
       ['a.b.c.d', '1.1.1.256', 'lorem ipsum']
     ]
-    for (const ipAddresses of badIPAddressesMatrix) { 
+    for (const ipAddresses of badIPAddressesMatrix) {
       await generalSettingsPage.fillAllowedIPAddresses(ipAddresses);
       await generalSettingsPage.save({ expectLoadingShowAndHide: false });
       await expect(page.getByText('This field must contain only valid IP addresses.'), 'The error message under "Allowed IP addresses" field should be visible').toBeVisible();
       await page.reload();
-      await generalSettingsPage.expectLoadingShowAndHide(); 
+      await generalSettingsPage.expectLoadingShowAndHide();
     }
 
     // Test with valid IP addresses
@@ -60,6 +60,55 @@ test.describe('Configuration', () => {
 
     for (const ipAddresses of allowedIPAddressesMatrix) {
       await fillAndAssert(ipAddresses, true);
+    }
+  });
+
+  test('Change excluded categories', async ({ page, generalSettingsPage, productPage, checkoutPage }) => {
+
+    await checkoutPage.setupForPhysicalProducts();
+
+    const allowedCategoriesMatrix = [
+      [],
+      ['Music']
+      ['Music', 'Uncategorized'],
+    ];
+
+    const notAllowedCategoriesMatrix = [
+      ['Accessories'],
+      ['Accessories', 'Music'],
+    ];
+
+    // Test cancellation of the changes
+    await generalSettingsPage.goto();
+    await generalSettingsPage.expectLoadingShowAndHide();
+    await generalSettingsPage.selectExcludedCategories(notAllowedCategoriesMatrix[0]);
+    await generalSettingsPage.cancel();
+    await generalSettingsPage.expectExcludedCategoriesToBeEmpty();
+
+
+    const fillAndAssert = async (categories, available) => {
+      await generalSettingsPage.selectExcludedCategories(categories);
+      if (categories) {
+        await generalSettingsPage.save({});
+      }
+
+      await generalSettingsPage.logout();
+
+      await productPage.addToCart({ slug: 'sunglasses', quantity: 1 });
+
+      await checkoutPage.goto();
+      await checkoutPage.expectAnyPaymentMethod({ available });
+
+      await generalSettingsPage.goto();
+      await generalSettingsPage.expectLoadingShowAndHide();
+    }
+
+    for (const categories of notAllowedCategoriesMatrix) {
+      await fillAndAssert(categories, false);
+    }
+
+    for (const categories of allowedCategoriesMatrix) {
+      await fillAndAssert(categories, true);
     }
   });
 });
