ec8d6734c145b5eb9b24aac80430eb818d99e5cd
Michel Escalante Álvarez <michel.escalante@sequra.com>
2024-10-22 16:02:37 +0200

    Define no address glue plugin

---
+++ glue-plugins/sequra-no-address/sequra-no-address.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * The plugin bootstrap file
+ *
+ * @package SeQura/WC
+ *
+ * @wordpress-plugin
+ * Plugin Name:       seQura - No Address Addon
+ * Plugin URI:        https://sequra.es/
+ * Description:       Allow customers to pay with seQura without providing an address.
+ * Version:           3.0.0-rc.3
+ * Author:            "seQura Tech" <wordpress@sequra.com>
+ * Author URI:        https://sequra.com/
+ * License:           GPL-3.0+
+ * License URI:       https://www.gnu.org/licenses/gpl-3.0.txt
+ * Text Domain:       sequra
+ * Domain Path:       /languages
+ * Requires PHP:      7.3
+ * Requires at least: 5.9
+ * Tested up to:      6.6.2
+ * seQura requires at least: 3.0.0-rc.3
+ * Requires Plugins:  woocommerce
+ */
+
+defined( 'WPINC' ) || die;
+
+require plugin_dir_path( __FILE__ ) . 'vendor/autoload.php';
+
+call_user_func(
+	function () {   
+		$plugin = new \SeQura\WC\NoAddress\Plugin();
+		register_deactivation_hook( __FILE__, array( $plugin, 'deactivate' ) );
+	}
+);
---
+++ glue-plugins/sequra-no-address/src/class-bootstrap.php
@@ -0,0 +1,106 @@
+<?php
+/**
+ * Implementation for the core bootstrap class.
+ *
+ * @package    SeQura/WC/NoAddress
+ */
+
+namespace SeQura\WC\NoAddress;
+
+use SeQura\Core\Infrastructure\ServiceRegister;
+
+/**
+ * Implementation for the core bootstrap class.
+ */
+class Bootstrap {
+
+	/**
+	 * Cache for Service instances.
+	 *
+	 * @var array<string, mixed>
+	 */
+	private static $cache = array();
+
+	/**
+	 * Initialize the bootstrap.
+	 */
+	public static function init(): void {
+		self::init_constants();
+		self::init_services();
+		self::init_controllers();
+	}
+
+	/**
+	 * Initializes constants.
+	 */
+	private static function init_constants(): void {
+		ServiceRegister::registerService(
+			'noaddress_addon.dir_path',
+			static function () {
+				if ( ! isset( self::$cache['noaddress_addon.dir_path'] ) ) {
+					self::$cache['noaddress_addon.dir_path'] = trailingslashit( dirname( __DIR__, 1 ) );
+				}
+				return self::$cache['noaddress_addon.dir_path'];
+			}
+		);
+		ServiceRegister::registerService(
+			'noaddress_addon.file_path',
+			static function () {
+				if ( ! isset( self::$cache['noaddress_addon.file_path'] ) ) {
+					self::$cache['noaddress_addon.file_path'] = ServiceRegister::getService( 'noaddress_addon.dir_path' ) . 'sequra-no-address.php';
+				}
+				return self::$cache['noaddress_addon.file_path'];
+			}
+		);
+		ServiceRegister::registerService(
+			'noaddress_addon.basename',
+			static function () {
+				if ( ! isset( self::$cache['noaddress_addon.basename'] ) ) {
+					self::$cache['noaddress_addon.basename'] = plugin_basename( ServiceRegister::getService( 'noaddress_addon.dir_path' ) . 'sequra-no-address.php' );
+				}
+				return self::$cache['noaddress_addon.basename'];
+			}
+		);
+		ServiceRegister::registerService(
+			'noaddress_addon.data',
+			static function () {
+				if ( ! isset( self::$cache['noaddress_addon.data'] ) ) {
+					if ( ! function_exists( 'get_plugin_data' ) ) {
+						require_once ABSPATH . 'wp-admin/includes/plugin.php';
+					}
+					$add_wc_headers = function ( $headers ) {
+						$headers['seQura requires at least'] = 'seQura requires at least';
+						return $headers;
+					};
+					add_filter( 'extra_plugin_headers', $add_wc_headers );
+					$data = get_plugin_data( ServiceRegister::getService( 'noaddress_addon.file_path' ) );
+					remove_filter( 'extra_plugin_headers', $add_wc_headers );
+					
+					$data['RequiresSQ'] = '';
+					if ( isset( $data['seQura requires at least'] ) ) {
+						$data['RequiresSQ'] = $data['seQura requires at least'];
+						unset( $data['seQura requires at least'] );
+					}
+
+					self::$cache['noaddress_addon.data'] = $data;
+				}
+				return self::$cache['noaddress_addon.data'];
+			}
+		);
+	}
+
+	/**
+	 * Initializes services and utilities.
+	 */
+	private static function init_services(): void {
+		// TODO: Implement init_services() method.
+	}
+
+	
+	/**
+	 * Initializes controllers.
+	 */
+	private static function init_controllers(): void {
+		// TODO: Implement init_controllers() method.
+	}
+}
