dc22592197dab655e58fdd0c18ef9fc6248c37f5
Michel Escalante Álvarez <michel.escalante@sequra.com>
2025-05-27 16:25:31 +0200

    Add migration tests for SeQuraOrderRepository; verify table preparation and data migration

--- sequra/tests/Repositories/SeQuraOrderRepositoryTest.php
+++ sequra/tests/Repositories/SeQuraOrderRepositoryTest.php
@@ -34,6 +34,7 @@ class SeQuraOrderRepositoryTest extends WP_UnitTestCase {
 	}
 
 	public function tear_down() {
+		$this->order_table->remove_table( true ); // Remove legacy table if exists.
 		$this->order_table->reset();
 	}
 
@@ -47,5 +48,40 @@ class SeQuraOrderRepositoryTest extends WP_UnitTestCase {
 
 		// Assert.
 		$this->assertEquals( count( $expected_ids ), count( array_intersect( $actual_ids, $expected_ids ) ) );
+	}
+
+	public function testPrepareTablesForMigration_happyPath_TablesArePrepared() {
+		// Setup.
+		$original_table_content = $this->order_table->get_all( false );
+
+		// Execute.
+		$result = $this->repository->prepare_tables_for_migration();
+
+		// Assert.
+		$this->assertTrue( $result );
+		
+		$table_content = $this->order_table->get_all( false );
+		$this->assertTrue( is_array( $table_content ) && empty( $table_content ) );
+
+		$legacy_table_content = $this->order_table->get_all( true );
+		$this->assertEquals( $original_table_content, $legacy_table_content );
+
+		$this->assertEquals( 4, $this->order_table->get_next_id_value( true ) ); // Verify if auto-increment is set correctly.
+	}
+
+	public function testMigrateNextRow_happyPath_DataIsMigrated() {
+		// Setup.
+		$original_table_content = $this->order_table->get_all( false );
+
+		// Execute.
+		$this->repository->prepare_tables_for_migration();
+		$this->repository->migrate_next_row();
+
+		// Assert.
+		$legacy_table_content = $this->order_table->get_all( true );
+		$this->assertEquals( array_slice( $original_table_content, 1 ), $legacy_table_content );
+		
+		$table_content = $this->order_table->get_all( false );
+		$this->assertEquals( array( $original_table_content[0] ), $table_content );
 	}
 }
