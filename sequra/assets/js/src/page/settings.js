83e377f54bc3ee60cc580fa91604965ddec983d1
Michel Escalante Álvarez <michel.escalante@sequra.es>
2025-08-14 17:23:46 +0200

    refactor: migrate imports from integration-core-ui to sequra-core-admin-fe

--- sequra/assets/js/src/page/settings.js
+++ sequra/assets/js/src/page/settings.js
@@ -1,27 +1,28 @@
-import '../../../integration-core-ui/js/ImagesProvider';
-import '../../../integration-core-ui/js/AjaxService';
-import '../../../integration-core-ui/js/TranslationService';
-import '../../../integration-core-ui/js/TemplateService';
-import '../../../integration-core-ui/js/UtilityService';
-import '../../../integration-core-ui/js/ValidationService';
-import '../../../integration-core-ui/js/ResponseService';
-import '../../../integration-core-ui/js/PageControllerFactory';
-import '../../../integration-core-ui/js/FormFactory';
-import '../../../integration-core-ui/js/StateUUIDService';
-import '../../../integration-core-ui/js/ElementGenerator';
-import '../../../integration-core-ui/js/ModalComponent';
-import '../../../integration-core-ui/js/DropdownComponent';
-import '../../../integration-core-ui/js/MultiItemSelectorComponent';
-import '../../../integration-core-ui/js/DataTableComponent';
-import '../../../integration-core-ui/js/PageHeaderComponent';
-import '../../../integration-core-ui/js/ConnectionSettingsForm';
-import '../../../integration-core-ui/js/WidgetSettingsForm';
-import '../../../integration-core-ui/js/GeneralSettingsForm';
-import '../../../integration-core-ui/js/OrderStatusMappingSettingsForm';
-import '../../../integration-core-ui/js/StateController';
-import '../../../integration-core-ui/js/OnboardingController';
-import '../../../integration-core-ui/js/PaymentController';
-import '../../../integration-core-ui/js/SettingsController';
+
+import 'sequra-core-admin-fe/dist/resources/js/ImagesProvider';
+import 'sequra-core-admin-fe/dist/resources/js/AjaxService';
+import 'sequra-core-admin-fe/dist/resources/js/TranslationService';
+import 'sequra-core-admin-fe/dist/resources/js/TemplateService';
+import 'sequra-core-admin-fe/dist/resources/js/UtilityService';
+import 'sequra-core-admin-fe/dist/resources/js/ValidationService';
+import 'sequra-core-admin-fe/dist/resources/js/ResponseService';
+import 'sequra-core-admin-fe/dist/resources/js/PageControllerFactory';
+import 'sequra-core-admin-fe/dist/resources/js/FormFactory';
+import 'sequra-core-admin-fe/dist/resources/js/StateUUIDService';
+import 'sequra-core-admin-fe/dist/resources/js/ElementGenerator';
+import 'sequra-core-admin-fe/dist/resources/js/ModalComponent';
+import 'sequra-core-admin-fe/dist/resources/js/DropdownComponent';
+import 'sequra-core-admin-fe/dist/resources/js/MultiItemSelectorComponent';
+import 'sequra-core-admin-fe/dist/resources/js/DataTableComponent';
+import 'sequra-core-admin-fe/dist/resources/js/PageHeaderComponent';
+import 'sequra-core-admin-fe/dist/resources/js/ConnectionSettingsForm';
+import 'sequra-core-admin-fe/dist/resources/js/WidgetSettingsForm';
+import 'sequra-core-admin-fe/dist/resources/js/GeneralSettingsForm';
+import 'sequra-core-admin-fe/dist/resources/js/OrderStatusMappingSettingsForm';
+import 'sequra-core-admin-fe/dist/resources/js/StateController';
+import 'sequra-core-admin-fe/dist/resources/js/OnboardingController';
+import 'sequra-core-admin-fe/dist/resources/js/PaymentController';
+import 'sequra-core-admin-fe/dist/resources/js/SettingsController';
 import '../core/AdvancedController'; // TODO: Migrate this to integration-core-ui
 
 document.addEventListener('DOMContentLoaded', () => {
