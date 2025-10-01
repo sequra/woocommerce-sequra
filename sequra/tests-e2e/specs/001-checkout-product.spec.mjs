b9db7d2d1484fdbd1ebb46fabf2a446669111204
Michel Escalante Álvarez <michel.escalante@sequra.es>
2025-10-01 14:16:52 +0200

    Fix test: complete a successful payment with Sequra

--- sequra/tests-e2e/specs/001-checkout-product.spec.mjs
+++ sequra/tests-e2e/specs/001-checkout-product.spec.mjs
@@ -16,27 +16,30 @@ test.describe('Product checkout', () => {
       await helper.executeWebhook({ webhook: checkout_version, args: [{ name: 'version', value: version }] });
       await checkoutPage.goto({force: true});
       await checkoutPage.fillForm({isShipping: version === 'blocks', ...shopper});
+      await checkoutPage.expectPaymentMethodsBeingReloaded();
       for (const paymentMethod of paymentMethods) {
         await checkoutPage.expectPaymentMethodToBeVisible(paymentMethod);
+        await checkoutPage.openAndCloseEducationalPopup(paymentMethod);
       }
     }
   });
 
-  // test('Complete a successful payment with SeQura', async ({ helper, dataProvider, productPage, checkoutPage }) => {
-  //   // Setup
-  //   const { dummy_config } = helper.webhooks;
-  //   await helper.executeWebhook({ webhook: dummy_config }); // Setup for physical products.
-  //   const shopper = dataProvider.shopper();
+  test('Complete a successful payment with SeQura', async ({ helper, dataProvider, productPage, checkoutPage }) => {
+    // Setup
+    const { dummy_config, checkout_version } = helper.webhooks;
+    await helper.executeWebhook({ webhook: dummy_config }); // Setup for physical products.
+    await helper.executeWebhook({ webhook: checkout_version, args: [{ name: 'version', value: 'blocks' }] }); // Use modern checkout.
+    const shopper = dataProvider.shopper();
 
-  //   // Execution
-  //   await productPage.addToCart({ slug: 'push-it-messenger-bag', quantity: 1 });
-  //   await checkoutPage.goto();
-  //   await checkoutPage.fillForm(shopper);
-  //   await checkoutPage.openAndCloseEducationalPopup({ product: 'i1' });
-  //   await checkoutPage.placeOrder({ ...shopper, product: 'i1' });
-  //   await checkoutPage.waitForOrderSuccess();
-  //   await checkoutPage.expectOrderHasTheCorrectMerchantId('ES', helper, dataProvider);
-  // });
+    // Execution
+    await productPage.addToCart({ slug: 'sunglasses', quantity: 1 });
+    await checkoutPage.goto();
+    await checkoutPage.fillForm({isShipping: true, ...shopper});
+    await checkoutPage.expectPaymentMethodsBeingReloaded();
+    await checkoutPage.placeOrder({ ...shopper, product: 'i1' });
+    await checkoutPage.waitForOrderSuccess();
+    await checkoutPage.expectOrderHasTheCorrectMerchantId('ES', helper, dataProvider);
+  });
 
   // test('Complete a successful payment with SVEA', async ({ helper, dataProvider, productPage, checkoutPage }) => {
   //   // Setup
