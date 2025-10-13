a38fd76d1e958b517b3a95f6dc183d806245e335
Michel Escalante Álvarez <michel.escalante@sequra.es>
2025-10-13 16:34:29 +0200

    refactor: Replace hardcoded UI versions with constants from DataProvider for better maintainability

--- sequra/tests-e2e/_specs/001-checkout-product.spec.js
+++ sequra/tests-e2e/_specs/001-checkout-product.spec.js
@@ -1,5 +1,6 @@
 import SeQuraHelper from '../_fixtures/SeQuraHelper';
 import { test, expect } from '../_fixtures/test';
+import DataProvider from '../fixtures/utils/DataProvider.mjs';
 
 test.describe('Product checkout', () => {
 
@@ -8,10 +9,10 @@ test.describe('Product checkout', () => {
     await productPage.addToCart({ slug: 'sunglasses', quantity: 1 });
 
     const helper = new SeQuraHelper(request, expect);
-    for (const version of ['classic', 'blocks']) {
+    for (const version of [DataProvider.UI_CLASSIC, DataProvider.UI_BLOCKS]) {
       await helper.executeWebhook({ webhook: helper.webhooks.CHECKOUT_VERSION, args: [{ name: 'version', value: version }] });
       await checkoutPage.goto();
-      if (version === 'blocks') {
+      if (version === DataProvider.UI_BLOCKS) {
         await checkoutPage.expectPaymentMethodsBeingReloaded();
       }
       await checkoutPage.expectI1ToBeVisible();
@@ -55,14 +56,4 @@ test.describe('Product checkout', () => {
     await checkoutPage.waitForOrderOnHold();
     await checkoutPage.expectOrderChangeTo({ toStatus: 'wc-cancelled' });
   });
-
-  // test('Make a payment attempt forcing a failure by changing the order payload amounts so it differs with the approved one.', async ({ page, productPage, checkoutPage }) => {
-  //   await checkoutPage.setupForPhysicalProducts();
-  //   await productPage.addToCart({ slug: 'sunglasses', quantity: 1 });
-  //   await checkoutPage.goto();
-  //   await checkoutPage.fillWithNonSpecialShopperName({});
-  //   await checkoutPage.expectPaymentMethodsBeingReloaded();
-  //   await checkoutPage.placeOrderUsingI1({ forceFailure: true });
-  //   await checkoutPage.waitForOrderFailure();
-  // });
 });
