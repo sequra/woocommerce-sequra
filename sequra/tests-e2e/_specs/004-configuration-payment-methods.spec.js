208184ae1f152c2de8276f7de7bf576091bb61d5
Michel Escalante Álvarez <michel.escalante@sequra.es>
2025-10-14 10:46:17 +0200

    Remove obsolete fixture files and related code

--- sequra/tests-e2e/_specs/004-configuration-payment-methods.spec.js
+++
@@ -1,7 +0,0 @@
-import { test } from '../../fixtures/test';
-
-test.describe('Configuration', () => {
-  test('Payment methods', async ({ paymentMethodsSettingsPage }) => {
-    await paymentMethodsSettingsPage.expectAvailablePaymentMethodsAreVisible({ merchant: 'dummy', countries: ['ES', 'PT', 'IT'] });
-  });
-});
