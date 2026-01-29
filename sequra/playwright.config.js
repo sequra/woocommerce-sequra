b1c704d605b75bb0dc5dbea7a2ab6235aa20b646
Michel Escalante Álvarez <michel.escalante@sequra.es>
2026-01-29 15:20:25 +0100

    chore: update CI workflows for E2E and PHP tests, enhance Playwright config, and remove obsolete test spec

--- sequra/playwright.config.js
+++ sequra/playwright.config.js
@@ -2,30 +2,22 @@
 const { defineConfig, devices } = require('@playwright/test');
 
 /**
- * Read environment variables from file.
- * https://github.com/motdotla/dotenv
- */
-// require('dotenv').config({ path: path.resolve(__dirname, '.env') });
-
-/**
  * @see https://playwright.dev/docs/test-configuration
  */
 module.exports = defineConfig({
   testDir: './tests-e2e/specs',
   outputDir: process.env.CI ? '/tmp/test-results' : './tests-e2e/test-results/',
-  // timeout: 5 * 60 * 1000, // 5 minutes
-  timeout: 1 * 60 * 1000, // 1 minute
+  timeout: 5 * 60 * 1000, // 5 minutes
   /* Run tests in files in parallel */
   fullyParallel: false,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  // retries: 1,
-  retries: 0,
+  retries: process.env.CI ? 1 : 0,
   /* Opt out of parallel tests on CI. */
   workers: 1,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'list',
+  reporter: process.env.CI ? 'dot' : 'list',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
