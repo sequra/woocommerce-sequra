eb6f087c61e414751f26bfd26077859eec0e45e6
Michel Escalante Álvarez <michel.escalante@sequra.es>
2026-02-05 13:54:16 +0100

    Add methods to retrieve shop products and map WooCommerce products to ShopProduct instances

--- sequra/src/Core/Implementation/BusinessLogic/Domain/Integration/Product/class-product-service.php
+++ sequra/src/Core/Implementation/BusinessLogic/Domain/Integration/Product/class-product-service.php
@@ -8,6 +8,7 @@
 namespace SeQura\WC\Core\Implementation\BusinessLogic\Domain\Integration\Product;
 
 use SeQura\Core\BusinessLogic\Domain\Integration\Product\ProductServiceInterface;
+use SeQura\Core\BusinessLogic\Domain\Product\Model\ShopProduct;
 
 /**
  * Product Service
@@ -49,5 +50,71 @@ class Product_Service implements ProductServiceInterface {
 	public function getProductCategoriesByProductId( string $productId ): array {
 		$_product = wc_get_product( (int) $productId );
 		return $_product ? array_map( 'strval', $_product->get_category_ids() ) : array();
+	}
+
+	/**
+	 * Gets all shop products with their basic information.
+	 *
+	 * @param int $page Page number for pagination starting from 1.
+	 * @param int $limit Number of products to return per page.
+	 * @param string $search Search term to filter products by name or SKU.
+	 *
+	 * @return ShopProduct[]
+	 */
+	public function getShopProducts( int $page, int $limit, string $search ): array {
+		$args = array(
+			'status'  => 'publish',
+			'limit'   => $limit,
+			'offset'  => ( $page - 1 ) * $limit,
+			'orderby' => 'name',
+			'order'   => 'ASC',
+			'return'  => 'objects',
+		);
+
+		if ( ! empty( $search ) ) {
+			$args['s'] = $search;
+		}
+
+		$products = wc_get_products( $args );
+
+		return array_map( array( $this, 'map_to_shop_product' ), $products );
+	}
+
+	/**
+	 * Gets shop products by their IDs.
+	 *
+	 * @param string[] $ids
+	 *
+	 * @return ShopProduct[]
+	 */
+	public function getShopProductByIds( array $ids ): array {
+		if ( empty( $ids ) ) {
+			return array();
+		}
+
+		$args = array(
+			'include' => array_map( 'intval', $ids ),
+			'limit'   => -1,
+			'return'  => 'objects',
+		);
+
+		$products = wc_get_products( $args );
+
+		return array_map( array( $this, 'map_to_shop_product' ), $products );
+	}
+
+	/**
+	 * Maps a WooCommerce product to a ShopProduct instance.
+	 *
+	 * @param \WC_Product $product WooCommerce product.
+	 *
+	 * @return ShopProduct
+	 */
+	private function map_to_shop_product( $product ): ShopProduct {
+		return new ShopProduct( 
+			(string) $product->get_id(), 
+			$product->get_sku(),  
+			$product->get_name()
+		);
 	}
 }
