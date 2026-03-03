85196a2f4122a04f908844e503103b31aa207934
Michel Escalante Álvarez <michel.escalante@sequra.es>
2026-03-03 14:15:58 +0100

    Refactor Store_Info_Service: improve code structure and readability by adding missing docblocks and restructuring active plugins retrieval

--- sequra/src/Core/Implementation/BusinessLogic/Domain/Integration/StoreInfo/class-store-info-service.php
+++ sequra/src/Core/Implementation/BusinessLogic/Domain/Integration/StoreInfo/class-store-info-service.php
@@ -17,10 +17,17 @@ use SeQura\WC\Services\Platform\Interface_Platform_Provider;
 class Store_Info_Service implements StoreInfoServiceInterface {
 
 	/**
+	 * Platform provider
+	 * 
 	 * @var Interface_Platform_Provider
 	 */
 	private $platform_provider;
 	
+	/**
+	 * Construct
+	 * 
+	 * @param Interface_Platform_Provider $platform_provider The platform provider
+	 */
 	public function __construct( Interface_Platform_Provider $platform_provider ) {
 		$this->platform_provider = $platform_provider;
 	}
@@ -34,9 +41,12 @@ class Store_Info_Service implements StoreInfoServiceInterface {
 		$platform       = $this->platform_provider->get();
 		$db_version     = trim( implode( ' ', array( ( $platform->getDbName() ?? '' ), ( $platform->getDbVersion() ?? '' ) ) ) );
 		$active_plugins = array();
-		foreach ( \get_option( 'active_plugins', array() ) as $plugin ) {
-			$plugin_data      = \get_plugin_data( WP_PLUGIN_DIR . '/' . $plugin, true, false );
-			$active_plugins[] = trim( implode( ' ', array( ( $plugin_data['Name'] ?? '' ), ( $plugin_data['Version'] ?? '' ) ) ) );
+		$opt_value      = \get_option( 'active_plugins', array() );
+		if ( \is_array( $opt_value ) ) {
+			foreach ( $opt_value as $plugin ) {
+				$plugin_data      = \get_plugin_data( WP_PLUGIN_DIR . '/' . $plugin, true, false );
+				$active_plugins[] = trim( implode( ' ', array( ( $plugin_data['Name'] ?? '' ), ( $plugin_data['Version'] ?? '' ) ) ) );
+			}
 		}
 
 		return new StoreInfo(
