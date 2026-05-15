6db9e30b7ce03454727f2e38269ceee9bb97d4ee
Michel Escalante Álvarez <michel.escalante@sequra.es>
2026-05-15 13:53:28 +0200

    Update version to 4.3.2 and enhance compatibility with WordPress 7.0.0 and WooCommerce 10.7.0

--- sequra/sequra.php
+++ sequra/sequra.php
@@ -8,7 +8,7 @@
  * Plugin Name:       seQura
  * Plugin URI:        https://sequra.es/
  * Description:       seQura payment gateway for WooCommerce
- * Version:           4.3.1
+ * Version:           4.3.2
  * Author:            "seQura Tech" <wordpress@sequra.com>
  * Author URI:        https://sequra.com/
  * License:           GPL-3.0+
@@ -17,9 +17,9 @@
  * Domain Path:       /languages
  * Requires PHP:      7.3
  * Requires at least: 5.9
- * Tested up to:      6.9.4
+ * Tested up to:      7.0.0
  * WC requires at least: 4.7.0
- * WC tested up to: 10.6.0
+ * WC tested up to: 10.7.0
  * Requires Plugins:  woocommerce
  */
 
